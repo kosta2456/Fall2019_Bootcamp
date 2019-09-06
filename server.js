9f0c64b3aa8f84d80bb2a7574c5bb8124562d2a0
Kevin Kosta <kevinkosta@Kevins-MacBook-Pro-2.local>
2019-09-06 14:15:37 -0400

    First commit

--- server.js
+++ server.js
@@ -7,12 +7,10 @@ var http = require('http'),
 var listingData, server;
 
 var requestHandler = function(request, response) {
-  var parsedUrl = url.parse(request.url);
-
+  
   /*
     Your request handler should send listingData in the JSON format as a response if a GET request 
     is sent to the '/listings' path. Otherwise, it should send a 404 error. 
-
     HINT: Explore the request object and its properties 
     HINT: Explore the response object and its properties
     https://code.tutsplus.com/tutorials/http-the-protocol-every-web-developer-must-know-part-1--net-31177
@@ -24,28 +22,54 @@ var requestHandler = function(request, response) {
     HINT: Explore the list of MIME Types
     https://developer.mozilla.org/en-US/docs/Web/HTTP/Basics_of_HTTP/MIME_types/Complete_list_of_MIME_types
    */
-};
 
-fs.readFile('listings.json', 'utf8', function(err, data) {
-  /*
-    This callback function should save the data in the listingData variable, 
-    then start the server. 
+  var parsedUrl = url.parse(request.url);
+  if (request.method == "GET" && parsedUrl.pathname == "/listings") {
 
-    HINT: Check out this resource on fs.readFile
-    //https://nodejs.org/api/fs.html#fs_fs_readfile_path_options_callback
-
-    HINT: Read up on JSON parsing Node.js
-   */
-
+    response.write(listingData);
+    
+    } else {
+    
+    response.statusCode = 404;
+    
+    response.write("Bad gateway error");
+    
+    }
+    
+    response.end();
+    
+    };
+  
     //Check for errors
   
 
    //Save the sate in the listingData variable already defined
-  
+  fs.readFile("listings.json", "utf8", function (err, data) {
 
-  //Creates the server
-  
-  //Start the server
+    /*
+    This callback function should save the data in the listingData variable, 
+    then start the server. 
+    HINT: Check out this resource on fs.readFile
+    //https://nodejs.org/api/fs.html#fs_fs_readfile_path_options_callback
+    HINT: Read up on JSON parsing Node.js
+   */
 
+    listingData = data;
+    
+    });
+    
+    //Creates the server
+    
+    server = http.createServer(requestHandler);
+    
+    //Start the server
 
-});
+    server.listen(port, function() {
+    
+    console.log("Server listening on: http://127.0.0.1:" + port);
+    
+    });
+    
+    
+    
+    
